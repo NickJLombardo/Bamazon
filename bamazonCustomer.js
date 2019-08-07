746a75200053059ba99dae7b899914737086a704
nick <lombardo15@temple.edu>
2019-08-07 11:18:33 -0400

    added prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,10 +12,10 @@ var connection = mysql.createConnection({
   connection.connect(function(err){
     if(err) throw err;
     // console.log('connected as id' + connection.threadId + "\n")
-    query()
+    display()
 });
 
-function query(){
+function display(){
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         const displayTable = new Table({
@@ -25,8 +25,49 @@ function query(){
             for(let i =0; i<res.length; i++){
                 displayTable.push([res[i].item_id, res[i].product_name,res[i].department_name, res[i].price, res[i].stock_quantity])
             }
+        
             console.log(displayTable.toString())
+        askQuestion()
     })
+
+}
+
+function askQuestion(){
+    inquirer.prompt([
+        {
+           name: "ID",
+           type: "input",
+           message: "What is the ID of the titem you would like to purchase? [Quit with Q]",
+           filter:Number
+         },
+         {
+           name: "Quantity",
+           type: "input",
+           message: "How many would you like?",
+           filter: Number
+         },
+    ])
+    .then(function(answer){
+         const quantity = answer.Quantity;
+         const idNumber = answer.ID;
+            productCheck(idNumber, quantity)
+    })
+}
+
+function productCheck(ID, quantity){
+
+ connection.query('SELECT * FROM products WHERE item_id =' + ID, function (err, res){
+        if (err) throw err;
+        if(quantity <=res[0].stock_quantity){
+            console.log("Good News your order is in stock")
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantity + " WHERE item_id = " + ID)
+
+        } else{
+            console.log("Sorry we do not have enough of " + res[0].product_name)
+        };
+        display()
+       
+  });
 
 }
 
