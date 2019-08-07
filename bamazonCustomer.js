46e31d1040efab3ab5ca81c722f2517b4ba06e46
nick <lombardo15@temple.edu>
2019-08-07 18:31:59 -0400

    bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -50,7 +50,7 @@ function askQuestion(){
     .then(function(answer){
          const quantity = answer.Quantity;
          const idNumber = answer.ID;
-            productCheck(idNumber, quantity)
+        productCheck(idNumber, quantity)
     })
 }
 
@@ -71,6 +71,10 @@ function productCheck(ID, quantity){
 
 }
 
+// function exit() {
+// 	console.log("\nThanks for stopping by! Have a good day.");
+// 	connection.end();
+// }
 
 
 
